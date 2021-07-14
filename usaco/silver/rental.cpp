8700be1cc85e823fb01811483c7c9ac16ec5d2cb
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-07-13 23:21:40 -0500

    Update rental.cpp

--- usaco/silver/rental.cpp
+++ usaco/silver/rental.cpp
@@ -17,7 +17,7 @@ void setIO(string name = "") { // name is nonempty for USACO file I/O
 }
 
 ll cows[MAXN];
-pair<ll,ll> shops[MAXN];
+pair<ll, ll> shops[MAXN];
 ll rent[MAXN];
 ll shopsgain[MAXN];
 ll rentgain[MAXN];
@@ -33,20 +33,20 @@ int main() {
 	for (int i = 0; i < n; i++) cin >> cows[i];
 	for (int i = 0; i < m; i++) cin >> shops[i].second >> shops[i].first; 
 	for (int i = 0; i < r; i++) cin >> rent[i];
-	sort(cows, cows+n, greater<ll>());
-	sort(shops, shops+m, cmp);
-	sort(rent, rent+r, greater<ll>());
+	sort(cows, cows + n, greater<ll>());
+	sort(shops, shops + m, cmp);
+	sort(rent, rent + r, greater<ll>());
 
 	// calculate total profit from selling milk from the
 	// first i cows that can produce the most with greedy
 	int j = 0;
 	for (int i = 0; i < n; i++) {
 		ll curr = cows[i];
-		shopsgain[i+1] = shopsgain[i];
+		shopsgain[i + 1] = shopsgain[i];
 		while (j < m && curr > 0) {
 			ll amt = min(shops[j].second, curr);
 			curr-= amt;
-			shopsgain[i+1]+= shops[j].first * amt;
+			shopsgain[i + 1]+= shops[j].first * amt;
 			shops[j].second-= amt;
 			if (shops[j].second == 0) j++;
 		}
@@ -58,8 +58,8 @@ int main() {
 	// and find the maximum across all total profits
 	ll ret = shopsgain[n];
 	for (int i = 1; i <= n; i++) {
-		rentgain[i] = rent[i-1] + rentgain[i-1];
-		ret = max(ret, rentgain[i] + shopsgain[n-i]);
+		rentgain[i] = rent[i - 1] + rentgain[i - 1];
+		ret = max(ret, rentgain[i] + shopsgain[n - i]);
 	}
 	cout << ret << '\n';
 }
