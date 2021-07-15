58f5b86d180f13c96b9ae400f625993decb86b40
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-07-14 19:33:40 -0500

    Add files via upload

---
+++ usaco/gold/paintbarn.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#define MAXN 205
+#define MOD 107
+#define ll long long
+#define mp make_pair
+#define sz(x) (int) (x).size() 
+#define pb push_back
+
+using namespace std;
+
+void setIO(string prob = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if (sz(prob)) {
+        freopen((prob + ".in").c_str(), "r", stdin);
+        freopen((prob + ".out").c_str(), "w", stdout);
+    }
+}
+
+int n, k;
+// paint tracks how many layers of paint cover each square, and kpsums tracks
+// how many squares have k or k-1 layers of paint in 2D prefix sums
+int paint[MAXN][MAXN], kpsums[MAXN][MAXN];
+// the two rectangles must be separated by a horizontal and/or vertical line,
+// so we track the best rectangle on each side of a dividing line i (in the code
+// below, we calculate the rectangles to the top and bottom of a horizontal line
+// y = i and the rectangles to the left and right of a vertical line x = i at
+// the same time). the top and bottom half are tracked by topbest and bottombest,
+// while the left and right half are tracked by leftbest and rightbest
+int topbest[MAXN], bottombest[MAXN], leftbest[MAXN], rightbest[MAXN];
+
+int rsum(int a, int b, int xlen, int ylen) {
+    return kpsums[a + xlen][b + ylen] - kpsums[a][b + ylen] - kpsums[a + xlen][b] + kpsums[a][b];
+}
+
+int main() {
+    setIO("paintbarn");
+    cin >> n >> k;
+    int x1, y1, x2, y2;
+    for (int i = 0; i < n; i++) {
+        // the input for the problem is actually only in the range [0, 199]
+        // since if they allow coordinates with x or y equal to 200 the
+        // editorial solution code fails
+        cin >> x1 >> y1 >> x2 >> y2;
+        x1++; y1++; x2++; y2++;
+        paint[x1][y1]++;
+        paint[x1][y2]--;
+        paint[x2][y1]--;
+        paint[x2][y2]++;
+    }
+    
+    int ret = 0, curramt = 0;
+    for (int i = 1; i <= 200; i++) {
+        for (int j = 1; j <= 200; j++) {
+            // 2D prefix sums give us how many layers of paints cover each square, and
+            // we only care about the squares covered with k or k - 1 layers of paint
+            paint[i][j]+= paint[i - 1][j] + paint[i][j - 1] - paint[i - 1][j - 1];
+            if (paint[i][j] == k) {
+                kpsums[i][j] = -1;
+                curramt++;
+            }
+            else if (paint[i][j] == k - 1) kpsums[i][j] = 1;
+        }
+    }
+
+    for (int i = 0; i < 200; i++) {
+        for (int j = 0; j < 200; j++) {
+            kpsums[i + 1][j + 1]+= kpsums[i][j + 1] + kpsums[i + 1][j] - kpsums[i][j];
+        }
+    }
+
+    for (int l = 0; l <= 200; l++) {
+        for (int len = 1; l + len <= 200; len++) {
+            // we are essentially reducing the maximum subrectangle sum problem to
+            // 1 dimension by fixing two boundaries (l and l + len) for one dimension,
+            // and then performing Kadane's Algorithm on the other dimension as usual.
+            // l and l + len are the x boundaries for topsum and bottomsum, while they
+            // are the y boundaries for leftsum and rightsum
+            int topsum = 0, bottomsum = 0, leftsum = 0, rightsum = 0;
+            for (int i = 1; i <= 200; i++) {
+                topsum = max(0, topsum + rsum(l, i - 1, len, 1));
+                bottomsum = max(0, bottomsum + rsum(l, 200 - i, len, 1));
+                leftsum = max(0, leftsum + rsum(i - 1, l, 1, len));
+                rightsum = max(0, rightsum + rsum(200 - i, l, 1, len));
+                
+                topbest[i] = max(max(topbest[i], topbest[i - 1]), topsum);
+                bottombest[i] = max(max(bottombest[i], bottombest[i - 1]), bottomsum);
+                leftbest[i] = max(max(leftbest[i], leftbest[i - 1]), leftsum);
+                rightbest[i] = max(max(rightbest[i], rightbest[i - 1]), rightsum);
+                ret = max(ret, max(topbest[i], max(bottombest[i], max(leftbest[i], rightbest[i]))));
+            }
+        }
+    }
+
+    // for (int i = 1; i <= 200; i++) {
+    //     topbest[i] = max(topbest[i], topbest[i - 1]);
+    //     bottombest[i] = max(bottombest[i], bottombest[i - 1]);
+    //     leftbest[i] = max(leftbest[i], leftbest[i - 1]);
+    //     rightbest[i] = max(rightbest[i], rightbest[i - 1]);
+    // }
+
+    for (int i = 1; i < 200; i++) {
+        ret = max(ret, max(topbest[i] + bottombest[200 - i], leftbest[i] + rightbest[200 - i]));
+    }
+
+    cout << ret + curramt << '\n';
+    return 0;
+}
