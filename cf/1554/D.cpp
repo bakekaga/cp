8ccebbef631f6986a3a6eb01ec466b2b2fe617b2
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-07-31 19:15:20 -0500

    Create D.cpp

---
+++ cf/1554/D.cpp
@@ -0,0 +1,34 @@
+// Handle: bakekaga
+// Problem: 1554D Diane
+// URL: https://codeforces.com/contest/1554/problem/D
+
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define MOD 107
+#define ll long long
+#define mp make_pair
+#define sz(x) (int) (x).size() 
+#define pb push_back
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    if (n == 1) cout << "a\n";
+    else {
+        for (int i = 0; i < n/2; i++) cout << "a";
+        if (n & 1) cout << "bc";
+        else cout << "b";
+        for (int i = 0; i < n/2 - 1; i++) cout << "a";
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
