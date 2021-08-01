28f858454bca9a6b1a893ff1592accf8dd8ef66a
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-07-31 19:14:21 -0500

    Create A.cpp

---
+++ cf/1554/A.cpp
@@ -0,0 +1,33 @@
+// Handle: bakekaga
+// Problem Name: 1554A Cherry
+// URL: https://codeforces.com/contest/1554/problem/A
+
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define MOD 107
+#define ll long long
+#define mp make_pair
+#define sz(x) (int) (x).size() 
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    ll last, n, cur, out = 0; cin >> n >> last;
+    for (int i = 1; i < n; i++) {
+        cin >> cur;
+        out = max(out, last * cur);
+        last = cur;
+    }
+    cout << out << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
