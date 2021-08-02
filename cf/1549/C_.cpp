e755fed8442765fea420c8197d775a42f27b3cce
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-08-01 20:51:04 -0500

    Create C_.cpp

---
+++ cf/1549/C_.cpp
@@ -0,0 +1,45 @@
+// Handle: bakekaga
+// Problem Name: 1549C Web of Lies
+// URL: https://codeforces.com/contest/1549/problem/C
+
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define MOD 107
+#define ll long long
+#define mp make_pair
+#define sz(x) (int) (x).size() 
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    int n, m, q, x, y, bad = 0; cin >> n >> m;
+    vector<int> cnt(n + 1);
+    for (int i = 0; i < m; i++) {
+        cin >> x >> y;
+        cnt[min(x, y)]++;
+        if (cnt[min(x, y)] == 1) bad++;
+    }
+
+    cin >> q;
+    while (q--) {
+        int id; cin >> id;
+        if (id == 1) {
+            cin >> x >> y;
+            cnt[min(x, y)]++;
+            if (cnt[min(x, y)] == 1) bad++;
+        }
+        else if (id == 2) {
+            cin >> x >> y;
+            cnt[min(x, y)]--;
+            if (cnt[min(x, y)] == 0) bad--;
+        }
+        else {
+            cout << n - bad << '\n';
+        }
+    }
+    return 0;
+}
