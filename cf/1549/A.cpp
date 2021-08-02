b41d95be3280acbd5b35f9c894c632f0e1a86caa
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-08-01 20:51:18 -0500

    Create A.cpp

---
+++ cf/1549/A.cpp
@@ -0,0 +1,28 @@
+// Handle: bakekaga
+// Problem Name: 1549A Gregor and Cryptography
+// URL: https://codeforces.com/contest/1549/problem/A
+
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define MOD 107
+#define ll long long
+#define mp make_pair
+#define sz(x) (int) (x).size() 
+#define pb push_back
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    ll p; cin >> p;
+    cout << 2 << ' ' << p - 1 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
