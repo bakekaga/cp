398af5359de9698071be1a31e400301f377bfe8b
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-08-12 16:19:15 -0500

    Create D.cpp

---
+++ cf/1549/D.cpp
@@ -0,0 +1,170 @@
+// Handle: bakekaga
+// Problem: 1549D Integers Have Friends
+// URL: https://codeforces.com/contest/1549/problem/D
+
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define MOD 107
+#define ll long long
+#define mp make_pair
+#define sz(x) (int) (x).size() 
+#define pb push_back
+
+using namespace std;
+
+ll gcd_impl (ll n, ll m) {
+    for (int i = 0; i < 10; ++i) {
+        ll t = n - m;
+        bool q = m > t;
+        n = q ? m : t;
+        m = q ? t : m;
+        if (m == 0) return n;
+    }
+    return gcd_impl(m, n % m);
+}
+
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+ 
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+ 
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+ 
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+ 
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+ 
+    S all_prod() const { return d[1]; }
+ 
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+ 
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+ 
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+ 
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+ll comb(ll a, ll b) {
+    return (a > b) ? gcd_impl(a, b) : gcd_impl(b, a);
+}
+
+ll e() {
+    return 0;
+}
+
+bool ok(ll x) {
+    return x != 1;
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<ll> arr(n), diff(n - 1);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n - 1; i++) {
+        diff[i] = abs(arr[i + 1] - arr[i]);
+    }
+    int best = 1;
+    segtree<ll, comb, e> st(diff);
+    for (int i = 0; i < n; i++) {
+        best = max(best, st.max_right<ok>(i) - i + 1);
+    }
+    cout << best << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
