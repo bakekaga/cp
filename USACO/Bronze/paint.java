8fee584966325beeba300bf31781ee64ef355bcd
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-09-10 15:48:05 -0500

    Add files via upload

---
+++ USACO/Bronze/paint.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class paint {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("paint.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("paint.out")));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		PrintWriter pw = new PrintWriter(System.out);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int c = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken());
+		pw.println(b - a + d - c - ((Math.max(a, c) < Math.min(b, d)) ? (Math.min(b, d) - Math.max(a, c)) : 0));
+		pw.close();
+	}
+}
