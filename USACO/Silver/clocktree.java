9527e5322f9288200146e26937675789c1d32d5b
bakekaga <55259825+bakekaga@users.noreply.github.com>
2021-12-14 08:31:41 -0600

    Add files via upload

---
+++ USACO/Silver/clocktree.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class clocktree {
+	static Node[] arr;
+	
+	static class Node {
+		public ArrayList<Integer> adj;
+		public int val, testval;
+		public Node(int v) {
+			val = testval = v;
+			adj = new ArrayList<Integer>();
+		}
+	}
+	
+	static int dfs(int cur, int prev) {
+		for (int node : arr[cur].adj) {
+			if (node == prev) continue;
+			arr[cur].testval+= 12 - dfs(node, cur);
+			arr[cur].testval%= 12;
+		}
+		return arr[cur].testval;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("clocktree.in"));
+		PrintWriter pw = new PrintWriter(new FileWriter("clocktree.out"));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new Node[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = new Node(Integer.parseInt(st.nextToken()));
+			if (arr[i].testval == 12) arr[i].testval = arr[i].val = 0;
+		}
+		for (int i = 0; i < n - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			arr[a - 1].adj.add(b - 1);
+			arr[b - 1].adj.add(a - 1);
+		}
+		
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			dfs(i, -1);
+			if (arr[i].testval == 0 || arr[i].testval == 1) cnt++;
+			for (int j = 0; j < n; j++) arr[j].testval = arr[j].val;
+		}
+		pw.println(cnt);
+		pw.close();
+	}
+}
