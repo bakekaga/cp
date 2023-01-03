c8fa598000b02d51263b4e07f917636e5ea25ac2
bakekaga <awesomealbs@gmail.com>
2023-01-03 10:02:03 -0600

    modified problem

--- CSES/2/FerrisWheel.cpp
+++ CSES/2/FerrisWheel.cpp
@@ -7,29 +7,26 @@ using namespace std;
  
 typedef long long ll;
  
-const int MAXN = 1e5 + 5;
+const int MAXN = 2e5 + 5;
 const int MOD = 1e9 + 7;
 const int INF = 0x3f3f3f3f;
 const ll INFLL = 0x3f3f3f3f3f3f3f3f;
 const double EPS = 1e-6;
 
-vector<int> adj[MAXN];
-bool vis[MAXN];
-int ind[MAXN];
-vector<int> ord;
+ll p[MAXN];
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	int n, cnt = 0; ll x;
 	cin >> n >> x;
-	vector<ll> p(n);
-	for (int i = 0; i < n; i++) cin >> p[i];
-	sort(p.begin(), p.end());
-	int i = 0, j = n - 1;
-	for (; i < j; j--) {
-		if (p[i] + p[j] <= x) i++;
+	for (int i = 0; i < n; i++) {
+		cin >> p[i];
+	}
+	sort(p, p + n);
+	for (int i = n - 1, j = 0; i >= j; i--) {
+		if (p[i] + p[j] <= x) j++;
 		cnt++;
 	}
-	cout << cnt + (j == i) << '\n';
+	cout << cnt << '\n';
 	return 0;
 }
