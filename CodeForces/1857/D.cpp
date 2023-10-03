10392dd316678494aa9032def1255cb00850257e
bakekaga <awesomealbs@gmail.com>
2023-10-03 14:24:29 -0500

    round 1857

---
+++ CodeForces/1857/D.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define mp make_pair
+#define sz(x) (int) (x).size() 
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+
+const int MAXN = 1e5 + 5;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll INFLL = 0x3f3f3f3f3f3f3f3f;
+const double EPS = 1e-6;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+// void __print(mi x) {cerr << x;}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<ll> a(n), b(n);
+	vector<ll> diff(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+		diff[i] = a[i] - b[i];
+	}
+	ll mx = *max_element(diff.begin(), diff.end());
+	vector<ll> out;
+	for (int i = 0; i < n; i++) {
+		if (diff[i] == mx) out.pb(i + 1);
+	}
+	cout << sz(out) << '\n';
+	for (auto i : out) cout << i << ' ';
+	cout << '\n';
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
